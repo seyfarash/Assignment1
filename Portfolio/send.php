bf09a1de44401ad676cd0f78e26bef43f4267ec3
Farshad Ashraf-Zadeh <seyfarash@gmail.com>
2014-09-28 20:55:02 -0400

    Added all content, social media buttons, contact form with php page.

---
+++ Portfolio/send.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Email Sent</title>
+</head>
+
+<body>
+
+
+<?php
+
+//store the user input in a variable
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+
+//submit a message to the email that was entered
+mail($email, $name, $message, 'From:200263939@student.georgianc.on.ca');
+
+
+?>
+
+
+</body>
+
+</html>
